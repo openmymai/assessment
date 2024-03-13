3cfc50b25a79ea9052e878b982a19788abd5db07
openmymai <maicmi@me.com>
2024-03-13 14:38:51 +0700

    Add createExpenseHandler feature

--- server.go
+++ server.go
@@ -3,13 +3,42 @@ package main
 import (
 	"database/sql"
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 
+	"github.com/lib/pq"
 	_ "github.com/lib/pq"
 )
+
+type Expense struct {
+	ID     int      `json:"id"`
+	Title  string   `json:"title"`
+	Amount float32  `json:"amount"`
+	Note   string   `json:"note"`
+	Tags   []string `json:"tags"`
+}
+
+type Err struct {
+	Message string `json:"message"`
+}
+
+func createExpenseHandler(c echo.Context) error {
+	e := Expense{}
+	err := c.Bind(&e)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, Err{Message: err.Error()})
+	}
+
+	row := db.QueryRow("INSERT INTO expenses (title, amount, note, tags) values ($1, $2, $3, $4) RETURNING id", e.Title, e.Amount, e.Note, pq.Array(e.Tags))
+	err = row.Scan(&e.ID)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, Err{Message: err.Error()})
+	}
+	return c.JSON(http.StatusCreated, e)
+}
 
 var db *sql.DB
 
@@ -34,8 +63,17 @@ func main() {
 
 	e := echo.New()
 
+	e.Use(middleware.BasicAuth(func(username, password string, c echo.Context) (bool, error) {
+		if username == "apidesign" || password == "45678" {
+			return true, nil
+		}
+		return false, nil
+	}))
+
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
+
+	e.POST("/expenses", createExpenseHandler)
 
 	log.Fatal(e.Start(os.Getenv("PORT")))
 }
